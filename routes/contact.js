23ecc7cb4317adeb14a829bea3772844ed7b82b3
sharon <sharonghae@gmail.com>
2016-08-05 17:36:12 -0400

    add hours, gift, and reservation info

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('contact', { title: 'Elite Nails' });
+    res.render('contact', { title: 'Elite Nails | Willow Park, TX' });
 });
 
 module.exports = router;
--- routes/services.js
+++ routes/services.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('services', { title: 'Elite Nails' });
+    res.render('services', { title: 'Elite Nails | Willow Park, TX' });
 });
 
 module.exports = router;
