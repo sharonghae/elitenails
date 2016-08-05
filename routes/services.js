75d97a3ed48747efa043c1af946a204fb28425d6
sharon <sharonghae@gmail.com>
2016-08-04 21:47:33 -0400

    initial commit

---
+++ routes/services.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Sharon on 8/3/2016.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('services', { title: 'Elite Nails' });
+});
+
+module.exports = router;
