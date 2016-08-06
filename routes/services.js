62c7de6d01b00846e1b30e2ba8c273eb263f1a7c
sharon <sharonghae@gmail.com>
2016-08-06 09:49:22 -0400

    fix price list columns

--- routes/services.js
+++ routes/services.js
@@ -4,13 +4,18 @@
 var express = require('express');
 var router = express.Router();
 var pl = require('../pricelist.json');
+var pl1 = require('../pricelist1.json');
+var pl2 = require('../pricelist2.json');
 
 /* GET services page */
 router.get('/', function(req, res, next) {
     res.render('services',
         {
             title: 'Elite Nails | Willow Park, TX',
-            priceList: pl
+            priceList: pl,
+            priceList1: pl1,
+            priceList2: pl2
+            
         }
     );
 });
